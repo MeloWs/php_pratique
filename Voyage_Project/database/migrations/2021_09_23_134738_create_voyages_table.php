893da28bf7a02b39def4a7ef4bb1544754b0af3b
Melodie STRZEMPEK <strmel@yahoo.fr>
2022-10-17 12:28:44 +0200

    clean of php projects, move repository php-pratique

---
+++ Voyage_Project/database/migrations/2021_09_23_134738_create_voyages_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVoyagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('voyages', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title');
+            $table->string('country');
+            $table->date('date');
+            $table->string('agency');
+            $table->text('resume');
+            $table->integer('cost');
+            $table->integer('rating');
+            $table->integer('views');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('voyages');
+    }
+}
