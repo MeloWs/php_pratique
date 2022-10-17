d99342c08a6b24e5e8cf76670c61f236f88a84b3
Melodie STRZEMPEK <strmel@yahoo.fr>
2022-10-17 12:21:01 +0200

    clean of php projects, move repository php-pratique

--- resources/views/showpost.blade.php
+++
@@ -1,7 +0,0 @@
-<div style="width:400px;padding:15px;text-align:center;">
-    <div> {{$post->id}}
-        <h2>{{$post->title}}</h2>
-    </div>
-    <div> {{$post->body}}</div>
-    <a href="{{route('post')}}">RETOUR</a>
-</div>
